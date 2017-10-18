8c2cdc6d180a62c0aeb2500ed30a1f90dc84d18b
itiswiki <32880445+itiswiki@users.noreply.github.com>
2017-10-17 18:46:01 -0600

    Create vrfy.py

---
+++ vrfy.py
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+import socket
+import sys
+
+if len(sys.argv) != 2:
+        print "Usage: vrfy.py <username>"
+        sys.exit(0)
+
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM) # Create a Socket
+connect=s.connect(('10.11.10.12',25))   #Connect to the server
+banner=s.recv(1024)                     # Receive the banner
+print banner
+s.send('VRFY ' + sys.argv[1] + '\r\n')
+results=s.recv(1024)
+print result
+s.close()

